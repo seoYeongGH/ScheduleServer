0d8655a5e0e595e6dd5b8652461bb3faa4f01d1b
seoyeongGH <syho252@naver.com>
2019-09-23 18:05:13 +0900

    Initial Commit

---
+++ ScheduleServer/src/com/schedule/web/UserServlet.java
@@ -0,0 +1,50 @@
+package com.schedule.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static com.schedule.web.Constant.DUP_ID;
+import static com.schedule.web.Constant.SUCCESS;
+
+import domain.UserDAO;
+
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/user.do")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private UserDAO dao;
+	
+    public UserServlet() {}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 dao = new UserDAO();
+		 PrintWriter out = response.getWriter();
+			
+			String doing = request.getParameter("doing");
+			System.out.println(doing);
+			if("chkId".equals(doing)) {
+				if(dao.chkIdDup(request.getParameter("id")))
+					out.print(DUP_ID);
+				else
+					out.print(SUCCESS);
+			}
+			else if("join".equals(doing)) {
+				Integer code = dao.insertUser(request.getParameter("id"), request.getParameter("password"),request.getParameter("name"),request.getParameter("email"));
+				out.print(code);
+			}
+	}
+
+}
