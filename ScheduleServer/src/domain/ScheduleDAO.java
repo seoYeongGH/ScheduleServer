6f993ad66f717bed3ecaf8bda858f915e3f8d08a
seoyeongGH <syho252@naver.com>
2020-04-05 22:41:04 +0900

    Modify -ScheduleDAO
    Every fuction is changed to use jdbcTemplate.

--- ScheduleServer/src/domain/ScheduleDAO.java
+++ ScheduleServer/src/domain/ScheduleDAO.java
@@ -13,6 +13,7 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
+import org.json.JSONException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -26,47 +27,34 @@ import static structure.Constant.DELETE_SUCCESS;
 import static structure.Constant.FOR_USER;
 
 public class ScheduleDAO {
-	static {
-		try {
-			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-			System.out.println("Connect");
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
+	
+	class OneSchedule{
+		String date;
+		String startTime;
+		String endTime;
+		String schedule;
+		
+		public OneSchedule(String date, String startTime, String endTime, String schedule) {
+			this.date = date;
+			this.startTime = startTime;
+			this.endTime = endTime;
+			this.schedule = schedule;
 		}
 	}
 	
-	DataSource dataSource;
 	private JdbcTemplate jdbcTemplate;
+	private RowMapper<OneSchedule> scheduleMapper;
 	
 	public ScheduleDAO() {
+		scheduleMapper = new RowMapper<OneSchedule>() {
+			public OneSchedule mapRow(ResultSet rs, int rowMap) throws SQLException{
+				return new OneSchedule(rs.getString("scheduledate"), rs.getString("starttime"), rs.getString("endtime"), rs.getString("schedule"));
+			}
+		};
 	}
 	
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
-		this.dataSource = dataSource;
-	}
-	
-	public Connection getConnection() {
-		Connection con = null;
-
-		try {
-			con = dataSource.getConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return con;
-	}
-
-	public void closeConnection(Connection con) {
-		if (con != null) {
-			try {
-				con.close();
-			} catch (Exception e) {
-			}
-		}
-
-		return;
 	}
 	
 	public int addSchedule(int groupNum, String date,String startTime, String endTime, String schedule) {
@@ -95,146 +83,132 @@ public class ScheduleDAO {
 	}                                                                                             
 	
 
-	public List getAllSch() {
-		Connection con  = null;
+	public ArrayList getAllSch() {
+		List<OneSchedule> scheduleList = new ArrayList<>();
+		
+		scheduleList = jdbcTemplate.query("select a.scheduledate,a.starttime,a.endtime,a.schedule from scheduletable a left outer join linktable b "
+				+ "on a.groupnum = b.groupnum where b.userid=? or a.userid=? order by a.scheduledate,a.starttime", 
+				new Object[] {USession.getInstance().getId(),USession.getInstance().getId()}, scheduleMapper);
+		
 		ArrayList<ScheduleObject> listObj = new ArrayList<ScheduleObject>();
 		
+		ScheduleObject schObj = new ScheduleObject();
+		ArrayList<String> startTimes = new ArrayList<String>();
+		ArrayList<String> endTimes = new ArrayList<String>();
+		ArrayList<String> schedules = new ArrayList<String>();
+
+		String beforeDate = null;
+		String currentDate = null;
+		
+		int scheduleSize = scheduleList.size();
+		
 		try {
-			con = getConnection();
-			String beforeDate = null;
-			String currentDate = null;
-			String sql = "select a.scheduledate,a.starttime,a.endtime,a.schedule from scheduletable a left outer join linktable b "
-					+ "on a.groupnum = b.groupnum where b.userid=? or a.userid=? order by a.scheduledate,a.starttime";
+		for(int i=0; i<scheduleSize; i++) {
+			OneSchedule current = scheduleList.get(i);
+			currentDate = current.date;
 			
-			PreparedStatement pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, USession.getInstance().getId());
-			pstmt.setString(2, USession.getInstance().getId());
-			
-			ResultSet rs = pstmt.executeQuery();
-			
-			ScheduleObject schObj = new ScheduleObject();
-			ArrayList<String> startTimes = new ArrayList<String>();
-			ArrayList<String> endTimes = new ArrayList<String>();
-			ArrayList<String> schedules = new ArrayList<String>();
-			
-			while(rs.next()) {				
-				currentDate = rs.getString("scheduledate");
-				
-				if(!currentDate.equals(beforeDate)) {
-					if(beforeDate != null) {
-						schObj.put("scheduledate",beforeDate);
-						schObj.put("startTimes", startTimes);
-						schObj.put("endTimes", endTimes);
-						schObj.put("schedules", schedules);
-						
-						listObj.add(schObj);
-						
-						startTimes.clear();
-						endTimes.clear();
-						schedules.clear();
+			if(!currentDate.equals(beforeDate)) {
+				if(beforeDate != null) {
+					schObj.put("scheduledate",beforeDate);
+					schObj.put("startTimes", startTimes);
+					schObj.put("endTimes", endTimes);
+					schObj.put("schedules", schedules);
 					
-						schObj = new ScheduleObject();
-						
-					}
-						beforeDate = currentDate;
-				}
+					listObj.add(schObj);
+					
+					startTimes.clear();
+					endTimes.clear();
+					schedules.clear();
 				
-				startTimes.add(rs.getString("starttime"));
-				endTimes.add(rs.getString("endtime"));
-				schedules.add(rs.getString("schedule"));
+					schObj = new ScheduleObject();
 				}
+				beforeDate = currentDate;
+			}
 			
+			startTimes.add(current.startTime);
+			endTimes.add(current.endTime);
+			schedules.add(current.schedule);
+		}
+		
+		if(beforeDate!=null) {
 			if(beforeDate.equals(currentDate)) {
 				schObj.put("scheduledate",currentDate);
 				schObj.put("startTimes", startTimes);
 				schObj.put("endTimes", endTimes);
 				schObj.put("schedules", schedules);
-			
-			}
-			listObj.add(schObj);
-			
-			startTimes.clear();
-			endTimes.clear();
-			schedules.clear();
 		
-		}catch(Exception e) {
+			}
+		
+			listObj.add(schObj);
+			}
+		}catch(JSONException e) {
 			System.out.println("GET_ALL_DATA_EXP: "+e.toString());
-		}finally {
-			closeConnection(con);
 		}
 		
-
 		return listObj;
 	}
 	
-	public List getGroupSch(int groupNum) {
-		Connection con  = null;
+	public ArrayList getGroupSch(int groupNum) {
+		List<OneSchedule> scheduleList = new ArrayList<>();
+		
+		scheduleList = jdbcTemplate.query("select scheduledate,starttime,endtime,schedule from scheduletable where groupnum=? order by scheduledate,starttime", 
+				new Object[] {groupNum}, scheduleMapper);
+		
 		ArrayList<ScheduleObject> listObj = new ArrayList<ScheduleObject>();
 		
+		ScheduleObject schObj = new ScheduleObject();
+		ArrayList<String> startTimes = new ArrayList<String>();
+		ArrayList<String> endTimes = new ArrayList<String>();
+		ArrayList<String> schedules = new ArrayList<String>();
+
+		String beforeDate = null;
+		String currentDate = null;
+		
+		int scheduleSize = scheduleList.size();
+		
 		try {
-			con = getConnection();
-			String beforeDate = null;
-			String currentDate = null;
-			String sql = "select scheduledate,starttime,endtime,schedule from scheduletable where groupnum=? order by scheduledate,starttime";
+		for(int i=0; i<scheduleSize; i++) {
+			OneSchedule current = scheduleList.get(i);
+			currentDate = current.date;
 			
-			PreparedStatement pstmt = con.prepareStatement(sql);
-			pstmt.setInt(1, groupNum);
-			
-			ResultSet rs = pstmt.executeQuery();
-			
-			ScheduleObject schObj = new ScheduleObject();
-			ArrayList<String> startTimes = new ArrayList<String>();
-			ArrayList<String> endTimes = new ArrayList<String>();
-			ArrayList<String> schedules = new ArrayList<String>();
-			
-			while(rs.next()) {				
-				currentDate = rs.getString("scheduledate");
-				
-				if(!currentDate.equals(beforeDate)) {
-					if(beforeDate != null) {
-						schObj.put("scheduledate",beforeDate);
-						schObj.put("startTimes", startTimes);
-						schObj.put("endTimes", endTimes);
-						schObj.put("schedules", schedules);
-						
-						listObj.add(schObj);
-						
-						
-						startTimes.clear();
-						endTimes.clear();
-						schedules.clear();
+			if(!currentDate.equals(beforeDate)) {
+				if(beforeDate != null) {
+					schObj.put("scheduledate",beforeDate);
+					schObj.put("startTimes", startTimes);
+					schObj.put("endTimes", endTimes);
+					schObj.put("schedules", schedules);
 					
-						schObj = new ScheduleObject();
-						
-					}
-						beforeDate = currentDate;
-				}
+					listObj.add(schObj);
+					
+					startTimes.clear();
+					endTimes.clear();
+					schedules.clear();
 				
-				startTimes.add(rs.getString("starttime"));
-				endTimes.add(rs.getString("endtime"));
-				schedules.add(rs.getString("schedule"));
+					schObj = new ScheduleObject();
 				}
+				beforeDate = currentDate;
+			}
 			
+			startTimes.add(current.startTime);
+			endTimes.add(current.endTime);
+			schedules.add(current.schedule);
+		}
+		
+		if(beforeDate!=null) {
 			if(beforeDate.equals(currentDate)) {
 				schObj.put("scheduledate",currentDate);
 				schObj.put("startTimes", startTimes);
 				schObj.put("endTimes", endTimes);
 				schObj.put("schedules", schedules);
-			
-			}
-			listObj.add(schObj);
-			
-			startTimes.clear();
-			endTimes.clear();
-			schedules.clear();
 		
-		}catch(Exception e) {
-			System.out.println("GET_ALL_DATA_EXP: "+e.toString());
-		}finally {
-			closeConnection(con);
+			}
+		
+			listObj.add(schObj);
+			}
+		}catch(JSONException e) {
+			System.out.println("GET_GROUP_SCH_EXP: "+e.toString());
 		}
 		
-
 		return listObj;
 	}
 	
